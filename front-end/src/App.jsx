625cb3476ec5a4ca97f319aa1e6a31f5f951a197
XiWorl <97926984+XiWorl@users.noreply.github.com>
2025-06-18 11:34:01 -0700

    Add test data for cards
    This commit adds data that's used to test card creation.

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (<>
-    <div>
+    <div id="body-content">
       <Header />
       <CardContainer />
     </div>
--- front-end/src/components/CardContainer.jsx
+++ front-end/src/components/CardContainer.jsx
@@ -1,5 +1,27 @@
-export function CardContainer() {
+import "../styles/CardContainer.css"
+
+let testData = [
+    {id: 1, title: "Board title", type: "Thank You", author: "John"},
+    {id: 2, title: "Here I am", type: "Thank You", author: "Jimmy"},
+    {id: 3, title: "Title", type: "Celebration", author: "Johnny"},
+]
+
+function Card(props) {
     return (
-      <h1>Container testing</h1>
+        <div>
+            <h1>{props.title}</h1>
+            <h2>{props.type}</h2>
+            <h3>{props.author}</h3>
+        </div>
     )
-  }
+}
+
+export function CardContainer(props) {
+    return (
+        <div id="card-container">
+            {testData.map(function(item) {
+                return <Card key={item.id}title={item.title} type={item.type} author={item.author} />
+            })}
+        </div>
+    )
+}
