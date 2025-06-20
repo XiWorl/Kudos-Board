529b31b5db026fb2aab3ed695b7e037fbcd37222
XiWorl <97926984+XiWorl@users.noreply.github.com>
2025-06-19 17:55:15 -0700

    Add get and post endpoints
    This commit adds get request endpoints for cards and boards. It also adds post endpoints for creating cards and boards.

--- back-end/api/db-model-prisma.js
+++ back-end/api/db-model-prisma.js
@@ -3,13 +3,6 @@ const { PrismaClient } = require('../generated/prisma')
 const prisma = new PrismaClient()
 
 module.exports = {
-    async main() {
-        const boards = [
-          { title: 'Luna', type: 'dog' },
-        ]
-
-        for (const board of boards) {
-            await prisma.Board.create({ data: board })
-        }
+    async putBoard() {
     }
 }
--- back-end/api/server.js
+++ back-end/api/server.js
@@ -1,35 +1,128 @@
 const express = require('express')
 const cors = require('cors')
 const helmet = require('helmet')
-const model = require("./db-model-prisma")
+const modelHelper = require("./db-model-prisma")
+const { PrismaClient } = require('../generated/prisma')
+const prisma = new PrismaClient()
 
 const server = express()
 server.use(helmet())
 server.use(express.json())
 server.use(cors())
 
-const { PrismaClient } = require('../generated/prisma')
 
-const prisma = new PrismaClient()
+function checkIfBoardIsValid(data) {
+	const isBoardValid = (
+		data.title !== undefined &&
+		data.author !== undefined &&
+		data.category !== undefined &&
+		data.imageUrl !== undefined
+	)
+	return isBoardValid
+}
+
+function checkIfCardIsValid(data) {
+	const isCardValid = (
+		data.message !== undefined &&
+		data.author !== undefined &&
+		data.gifUrl !== undefined &&
+		data.upvotes !== undefined &&
+		data.boardId !== undefined
+	)
+	return isCardValid
+}
 
 
-server.get("/api", async (req, res, next) => {
-    try {
-        const data = await prisma.board.findMany()
-        res.status(200).json(data)
-        return data
+server.get("/api/boards", async (req, res, next) => {
+	try {
+		const data = await prisma.board.findMany()
+		res.status(200).json(data)
+		return
 
-    } catch (err) {
-      next(err)
-    }
-    next({ message: "GET /api", status: 200 })
+	} catch (err) {
+		next(err)
+		return
+	}
 })
 
+server.get("/api/boards/:boardId", async (req, res, next) => {
+	try {
+		const id = parseInt(req.params.boardId)
+		const data = await prisma.board.findUnique({ where: { id: id } })
+		res.status(200).json(data)
+		return
+
+	} catch (err) {
+		next(err)
+		return
+	}
+})
+
+server.get("/api/cards", async (req, res, next) => {
+	try {
+		const data = await prisma.card.findMany()
+		res.status(200).json(data)
+		return
+
+	} catch (err) {
+		next(err)
+		return
+	}
+})
+
+server.get("/api/cards/:cardId", async (req, res, next) => {
+	try {
+		const id = parseInt(req.params.cardId)
+		const data = await prisma.card.findUnique({ where: { id: id } })
+		res.status(200).json(data)
+		return
+
+	} catch (err) {
+		next(err)
+		return
+	}
+})
+
+server.post("/api/boards", async (req, res, next) => {
+	const newBoard = req.body
+
+	try {
+		if (checkIfBoardIsValid(newBoard) == true) {
+			const data = await prisma.board.create({ data: newBoard })
+			res.status(201).json(data)
+			return
+		} else {
+			res.status(400).json({ message: "Invalid board data" })
+			return
+		}
+	} catch (err) {
+		next(err)
+		return
+	}
+})
+
+server.post("/api/boards/:boardId/cards", async (req, res, next) => {
+	const newCard = req.body
+	
+	try {
+		if (checkIfCardIsValid(newCard) == true) {
+			const data = await prisma.card.create({ data: newCard })
+			res.status(201).json(data)
+			return
+		} else {
+			res.status(400).json({ message: "Invalid card data" })
+			return
+		}
+	} catch (err) {
+		next(err)
+		return
+	}
+})
 
 // Error handling middleware
 server.use((err, req, res, next) => {
-  const { message, status = 500 } = err
-  res.status(status).json({ message }) // Unsafe in prod
+	const { message, status = 500 } = err
+	res.status(status).json({ message }) // Unsafe in prod
 })
 
 module.exports = server
