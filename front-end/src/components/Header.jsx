2764421a94ebb8d720bd55b77a83a3add4c61c8c
XiWorl <97926984+XiWorl@users.noreply.github.com>
2025-06-19 19:12:57 -0700

    Improve header html

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,5 +1,20 @@
 export function Header() {
     return (
-        <h1>Header test</h1>
+        <>
+            <h1>Kudos Board</h1>
+            <div id="search-div">
+                <input type="text" id="search" placeholder="Search" />
+                <button id="search-button" className="searchbar-button">Search</button>
+                <button id="clear-search" className="searchbar-button">Clear</button>
+            </div>
+            <div id="filter-div">
+                <button className="filter-button">All</button>
+                <button className="filter-button">Recent</button>
+                <button className="filter-button">Celebration</button>
+                <button className="filter-button">Thank You</button>
+                <button className="filter-button">Inspiration</button>
+            </div>
+            <button id="create-a-board" className="filter-button">Create A New Board</button>
+        </>
     )
 }
